3ef453e695c4ce8c26e8cc247696383b46116158
Uladzimir Bely <vladzimir.bely@gmail.com>
2024-03-13 14:53:05 +0300

    add google_should_find_selene.py with modified test

---
+++ google_should_find_selene.py
@@ -0,0 +1,10 @@
+import pytest
+from selene import browser, be, have
+
+@pytest.fixture()
+def open_browser():
+    browser.open('https://google.com')
+
+def test_search(open_browser):
+    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+    browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
