a9955fe60114d93a7940e3fcf846abf7b485b945
Uladzimir Bely <vladzimir.bely@gmail.com>
2024-03-13 16:05:12 +0300

    add fixture size browser

--- google_should_find_selene.py
+++ google_should_find_selene.py
@@ -2,9 +2,12 @@ import pytest
 from selene import browser, be, have
 
 @pytest.fixture()
+def size_browser():
+    browser.driver.set_window_size(1920, 1500)
+@pytest.fixture()
 def open_browser():
     browser.open('https://google.com')
 
-def test_search(open_browser):
+def test_search(size_browser, open_browser):
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
